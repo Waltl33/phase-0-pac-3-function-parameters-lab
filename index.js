268a9c8d2fd1fbe6968aaa3942c4c9de929784df
Walter <walter.clark24@yahoo.com>
2022-11-07 15:17:12 -0500

    commit tree e20b45b73d16fc13aded1a992cde9192bb163869

---
+++ index.js
@@ -0,0 +1,15 @@
+function introduction(firstName) {
+   return `Hi, my name is ${firstName}.`
+  }
+  introduction("Aki")
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+    introductionWithLanguage("Aki","Ember.js")
+}
+function introductionWithLanguageOptional(name, language ="Javascript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+
+}
+introductionWithLanguageOptional("Gracie")
+
+    
